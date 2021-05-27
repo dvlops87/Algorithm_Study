d63289552de036f15fcccf128eb39c899e14a347
happymink <71982555+happymink@users.noreply.github.com>
2021-05-27 13:25:30 +0900

    new file

--- src/comu/HelloWorld.java
+++ src/comu/HelloWorld.java
@@ -2,6 +2,15 @@ package comu;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("코뮤니티 사랑해요");
+        String name = "균민이";
+        int age = 24;
+        double height = 176.5;
+        char blood_type = 'B';
+        boolean girlfriend = false;
+        System.out.println("String name : " + name);
+        System.out.println("int age : " + age);
+        System.out.println("double height :" + height);
+        System.out.println("char blood_type :" + blood_type);
+        System.out.println("boolean girlfriend :" + girlfriend);
     }
 }
