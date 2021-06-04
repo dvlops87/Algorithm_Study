ce1558eeb1e474eda7d81d3863f59ce85b8a68da
happymink <71982555+happymink@users.noreply.github.com>
2021-06-04 14:51:45 +0900

    day 10

---
+++ src/comu/TotalSum.java
@@ -0,0 +1,28 @@
+package comu;
+
+import java.util.Scanner;
+
+public class TotalSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력하세요: ");
+        int num = sc.nextInt();
+        int total = 0;
+
+        /* for문으로 구현 */
+        for (int i = 1; i<=num; i++){
+            total += i;
+        }
+        System.out.printf("1부터 %d까지의 합은 %d입니다! \n",num, total);
+
+        /* while문으로 구현 */
+        int j = 0;
+        total = 0;
+        while(j <= num){
+            total += j;
+            j++;
+        }
+        System.out.printf("1부터 %d까지의 합은 %d입니다! ",num, total);
+        sc.close();
+    }
+}
