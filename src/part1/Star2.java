5998d8e85596ab7ae1ab32b938cd0b40096f8a29
happymink <71982555+happymink@users.noreply.github.com>
2021-06-06 14:40:52 +0900

    별찍기 2

---
+++ src/part1/Star2.java
@@ -0,0 +1,22 @@
+package part1;
+
+import java.util.Scanner;
+
+public class Star2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 1; i<=num ; i++){
+            for (int j = num; j>i ; j--){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
