11834c546bb4bd2bc32b3981d06176f76eccb04a
happymink <71982555+happymink@users.noreply.github.com>
2021-06-14 23:07:01 +0900

    9614

--- src/jungsuk/ArrayEx5.java
+++ src/jungsuk/ArrayEx5.java
@@ -1,15 +1,22 @@
 package jungsuk;
 
+import java.util.Scanner;
+
 public class ArrayEx5 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         int sum = 0; //총점 저장
         float average = 0f; //평균 저장
 
-        int [] score = {100, 88, 100, 100, 90};
+        int [] score = new int[5];
 
         for (int i = 0; i < score.length; i++){
+            System.out.print("점수를 입력하세요: ("+(i+1)+")번 입력");
+            score[i] = sc.nextInt();
             sum += score[i];
         }
+
     average = sum/(float)score.length;
 
         System.out.println("총점 : " + sum);
