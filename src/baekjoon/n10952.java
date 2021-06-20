31904d0646b8b6915c44e4ea2bd307825755fd94
happymink <71982555+happymink@users.noreply.github.com>
2021-06-20 23:25:46 +0900

    ch11

--- src/baekjoon/n10952.java
+++ src/baekjoon/n10952.java
@@ -6,7 +6,7 @@ public class n10952 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 =0, num2 = 0;
-        int[] result = new int[];
+
         while(num1 == 0 && num2 == 0){
             num1 = sc.nextInt();
             num2 = sc.nextInt();
