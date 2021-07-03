2c4f3ee56cc75bd0e643745724583c93d9f2252a
jjimini98 <jjimini98@naver.com>
2021-07-03 15:00:25 +0900

    2주차 완료

--- 지민/2주차/code_1978.py
+++ 지민/2주차/code_1978.py
@@ -1,19 +1,38 @@
 #1978 소수찾기 
 
-count = int(input())
-
-lis = list(map(int, input().split(" ")))
-
-for i in lis:
-    if i == 1 : lis[lis.index(i)] = 0 
-    for x in range(2,i):
-        if i%x == 0:
-            lis[lis.index(i)] = 0 
-            if i not in lis: break 
+num = int(input())
+lis = list(map(int,input().split(" ")))
 
 
-for x in lis :
-    if x == 0 : 
-        lis.remove(x)
+count = 0
 
-print(len(lis))
+for x in lis:
+    if x!=1:
+        share = []
+        for y in range(1,x+1):
+            if x%y ==0:
+               share.append(y)
+        if len(share) == 2:
+            count+=1
+
+print(count)
+
+
+
+# count = int(input())
+
+# lis = list(map(int, input().split(" ")))
+
+# for i in lis:
+#     if i == 1 : lis[lis.index(i)] = 0 
+#     for x in range(2,i):
+#         if i%x == 0:
+#             lis[lis.index(i)] = 0 
+#             if i not in lis: break 
+
+
+# for x in lis :
+#     if x == 0 : 
+#         lis.remove(x)
+
+# print(len(lis))
