4309e55fe434282dec66a8b2624cff3fb9f8e067
happymink <71982555+happymink@users.noreply.github.com>
2021-07-21 16:23:34 +0900

    5주차

---
+++ 민균/5주차/code_10950.java
@@ -0,0 +1,17 @@
+package baekjoon.study.week5;
+
+import java.util.Scanner;
+
+public class code_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 0; i<T; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
---
+++ 민균/5주차/code_2557.java
@@ -0,0 +1,7 @@
+package baekjoon.study.week5;
+
+public class code_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
