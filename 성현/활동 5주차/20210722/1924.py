45de79ebbead25e04be187d62a060de7ed3da2ca
성현 <tjdgus1977@gmail.com>
2021-07-23 02:01:35 +0900

    활동 5주차

---
+++ 성현/활동 5주차/20210722/1924.py
@@ -0,0 +1,10 @@
+x,y = map(int,input().split())
+Day = 0
+arrList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekList = ["SUN", "MON","TUE", "WED", "THU", "FRI", "SAT"]
+ 
+for i in range(x-1):
+    Day = Day + arrList[i]
+Day = (Day + y) % 7
+ 
+print(weekList[Day])
---
+++ 성현/활동 5주차/20210723/10818.py
@@ -0,0 +1,4 @@
+N = int(input())
+num = list(map(int, input().split()))
+num.sort()
+print(num[0], num[-1])
