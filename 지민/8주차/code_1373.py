638c3660bc3e2e6287b39f18d63af8efb98782ba
jjimini98 <jjimini98@naver.com>
2021-08-13 23:32:27 +0900

    1373 진행중..(실패)

--- 지민/8주차/code_1373.py
+++ 지민/8주차/code_1373.py
@@ -3,20 +3,33 @@ N = input()
 modi_N = len(N)%3 
 
 if modi_N == 1:
-	N = "00"+N
+	N = ''.join(reversed("00"+N))
 	
 elif modi_N ==2 : 	
-	N = "0"+N
-	
+	N = ''.join(reversed("0"+N))
+
+
+
 result = str()
 
-for n in range(len(N)):
-	if n % 3 ==1 : 
-		n  = n*(2**1) 
-	elif n % 3 == 2:
-		n = n*(2**2)
-	else:
-		n = n*(2**3)
+for x in range(1, len(N)+1):
+	if x%3 == 0 :
+		result += str(int(N[x-1]) *(2**2))
+	elif x%3 == 1 :
+		result += str(int(N[x-1])*1)
+	elif x%3 == 2 : 
+		result += str(int(N[x-1])* (2**1))
+
+resu = int()
+res = str() 
+
+for x in range(1, len(result)+1):
+	resu += int(result[x-1])
+	if x %3 ==0:
+		res += str(resu)
+		resu =0 
+
+print(''.join(reversed(res)))
 	
 	
 	
