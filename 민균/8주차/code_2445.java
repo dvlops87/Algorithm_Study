88bd05f244bbf6efe5e8c658a672c5abf062d343
happymink <71982555+happymink@users.noreply.github.com>
2021-08-15 17:04:20 +0900

    8week

---
+++ 민균/8주차/code_2445.java
@@ -0,0 +1,10 @@
+package baekjoon.study.week8;
+
+import java.util.Scanner;
+
+public class code_2445 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+    }
+}
